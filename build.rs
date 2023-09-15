cd8e5d2ceb26d329c562357ab976370515aba6dd
adakite1 <adakite.a@gmail.com>
2023-09-16 03:27:05 +0800

    Remove APIs unrelated to `adpcm-xq`

--- build.rs
+++ build.rs
@@ -1,21 +1,15 @@
-use std::path::PathBuf;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    cc::Build::new()
-        .file("adpcm-xq-nds/adpcm-xq.c")
-        .file("adpcm-xq-nds/adpcm-lib.c")
-        .compile("adpcm-xq-nds");
-    cc::Build::new()
-        .cpp(true)
-        .include(PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("r8brain-free-src"))
-        .file("r8brain-free-src/r8bbase.cpp")
-        .file("src/resampler-extern.cpp")
-        .compile("resampler");
-    // cc::Build::new()
-    //     .include(PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?).join("adpcm-xq"))
-    //     .file("src/lib.c")
-    //     .compile("dse-dsp-sys");
-    println!("cargo:rerun-if-changed=src/resampler-extern.cpp");
+    if cfg!(feature = "nds") {
+        cc::Build::new()
+            .file("adpcm-xq-nds/adpcm-xq.c")
+            .file("adpcm-xq-nds/adpcm-lib.c")
+            .compile("adpcm-xq-nds");
+    } else {
+        cc::Build::new()
+            .file("adpcm-xq/adpcm-xq.c")
+            .file("adpcm-xq/adpcm-lib.c")
+            .compile("adpcm-xq");
+    }
     println!("cargo:rerun-if-changed=src/lib.c");
     Ok(())
 }
